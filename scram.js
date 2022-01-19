a0277d3cf6149e18919b85ad2d74266ad24e5574
Ilham Taufiq <65374686+iamilham@users.noreply.github.com>
2022-01-20 06:37:18 +0700

    Update scram.js

--- scram.js
+++ scram.js
@@ -76,7 +76,7 @@ const fx = new TextScramble(el)
 let counter = 0
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 1500)
+    setTimeout(next, 3000)
   })
   counter = (counter + 1) % phrases.length
 }
